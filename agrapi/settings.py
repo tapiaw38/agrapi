53eb3e8f792ae11804357191a65088ced8768e9b
tapia <tapiaw38@gmail.com>
2021-01-12 01:28:45 -0300

    filter5

--- agrapi/settings.py
+++ agrapi/settings.py
@@ -160,6 +160,5 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    )
 }
