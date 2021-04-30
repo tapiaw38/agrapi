acb6fb86a70bb1640071f93b5a377081c78ded57
tapia <tapiaw38@gmail.com>
2021-04-29 21:11:25 -0300

    delete email

--- producer/serializers/producer.py
+++ producer/serializers/producer.py
@@ -752,7 +752,6 @@ class ProducerSerializer(WritableNestedModelSerializer):
             'gender',
             'phone_number',
             'another_phone_number',
-            'enail',
             'producer_home',
             'producer_person',
             'producer_vehicle',
